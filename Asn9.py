854dd896d73e612d809930fc3f6be8436861ea07
Parth <137251828+rabbitb8mile@users.noreply.github.com>
2023-07-03 22:09:11 +0530

    Add files via upload

---
+++ Asn9.py
@@ -0,0 +1,15 @@
+from cls import MathOperations
+import math as m
+# Perform math operations
+print("Math Operations:")
+print(MathOperations.add(5, 3))
+print(MathOperations.subtract(10, 4))
+print(MathOperations.multiply(6, 7))
+print(MathOperations.divide(15, 1))
+a=5.1
+print(m.ceil(a))
+b=5.99
+print(m.floor(b))
+print(m.sin(45))
+print(m.fsum([1,2,3,4,5]))
+print(m.pow(2,3))
